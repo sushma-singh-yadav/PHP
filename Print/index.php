9e26a18041788f5ea812cfa0f0a09c347f5e3998
Sushma <sushma2219@gmail.com>
2019-11-12 21:17:22 +0530

    Print Page in PHP
    Print Page in PHP

---
+++ Print/index.php
@@ -0,0 +1,68 @@
+<?php
+include 'database.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>PHP Print</title>
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <h2>User Data</h2>
+      <table class="table table-bordered print">
+        <thead>
+          <tr>
+            <th>S.No</th>
+            <th>User</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          $sn=1;
+          $user_qry="SELECT * from user";
+          $user_res=mysqli_query($con,$user_qry);
+          while($user_data=mysqli_fetch_assoc($user_res))
+          {
+          ?>
+          <tr>
+            <td><?php echo $sn; ?></td>
+            <td><?php echo $user_data['username']; ?></td>
+            <td><?php echo $user_data['email']; ?></td>
+          </tr>
+          <?php
+          $sn++;
+          }
+          ?>
+        </tbody>
+      </table>
+
+      <div class="text-center">
+        <a href="user_data_print.php" class="btn btn-primary">Print</a>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
