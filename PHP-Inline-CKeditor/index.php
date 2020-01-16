70ff7b09f31714dee44343c2c0e800f22aa69ac1
Sushma <sushma2219@gmail.com>
2020-01-16 22:16:37 +0530

    Create index.php

---
+++ PHP-Inline-CKeditor/index.php
@@ -0,0 +1,43 @@
+<?php
+include 'database.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>PHP Tutorial</title>
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h2>PHP Tutorial</h2>
+        <hr>
+        <form method="post" action="indexdb.php">
+          <div class="form-group row">
+            <label class="col-md-4">Description</label>
+            <div class="col-md-8">
+              <textarea name="description" id="editor1" contenteditable="true"></textarea>
+            </div>
+          </div>
+
+          <div class="form-group row">
+            <label class="col-md-4"></label>
+            <div class="col-md-8">
+              <input type="submit" name="submit" class="btn btn-primary">
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</body>
+<script>
+  CKEDITOR.disableAutoInline = true;
+  CKEDITOR.inline( 'editor1' );
+</script>
+</html>
+
