5897f4334eb1d66862f57651bfd75bb9bf971d18
Sushma <sushma2219@gmail.com>
2020-08-24 23:08:04 +0530

    Add files via upload

---
+++ DynamicMetaTag/metatag-add.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<?php include 'head.php';?>
+<body>
+<?php include 'menu.php';?>
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<h2>Add Meta Tag</h2>
+<hr>
+<form method="post" action="metatag-adddb.php">
+<div class="form-group">
+<label>Page</label>
+<input type="text" name="metatag_url" class="form-control">
+<select name="metatag_url" class="form-control">
+	
+</select>
+</div>
+<div class="form-group">
+<label>Title</label>
+<input type="text" name="metatag_title" class="form-control">
+</div>
+<div class="form-group">
+<label>Keywords</label>
+<textarea name="metatag_keywords" class="form-control"></textarea>
+</div>
+<div class="form-group">
+<label>Description</label>
+<textarea name="metatag_description" class="form-control"></textarea>
+</div>
+<input type="submit" name="submit" class="btn btn-primary">
+</form>
+</div>
+</div>
+</div>
+<?php include 'footer.php';?>
+</body>
+</html>
